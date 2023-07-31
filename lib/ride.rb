c8d3828fd79ffef8891a121eb38cdabccf0331f3
Tyler Blackmon <tyler.blackmon8@gmail.com>
2023-07-31 09:26:53 -0600

    feat: add total_revenue attribute to ride

--- lib/ride.rb
+++ lib/ride.rb
@@ -2,12 +2,14 @@ class Ride
   attr_reader :name,
               :min_height,
               :admission_fee,
-              :excitement
-              
+              :excitement,
+              :total_revenue
+
   def initialize(ride_details)
     @name = ride_details[:name]
     @min_height = ride_details[:min_height]
     @admission_fee = ride_details[:admission_fee]
     @excitement = ride_details[:excitement]
+    @total_revenue = 0
   end
 end
--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -17,5 +17,11 @@ RSpec.describe Ride do
       expect(ride1.admission_fee).to eq(1)
       expect(ride1.excitement).to eq(:gentle)
     end
+
+    it "has a total revenue that starts at 0" do
+      ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+
+      expect(ride1.total_revenue).to eq(0)
+    end
   end
 end
