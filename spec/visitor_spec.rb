b8a251d9a039cf93f36c43d1bda288d1db573cd4
Tyler Blackmon <tyler.blackmon8@gmail.com>
2023-07-31 09:15:29 -0600

    feat: add preferences to visitor

--- spec/visitor_spec.rb
+++ spec/visitor_spec.rb
@@ -15,5 +15,22 @@ RSpec.describe Visitor do
       expect(visitor1.height).to eq(54)
       expect(visitor1.spending_money).to eq("$10")
     end
+
+    it "has preferences that start empty" do
+      visitor1 = Visitor.new('Bruce', 54, '$10')
+
+      expect(visitor1.preferences).to be_empty
+    end
+  end
+
+  describe "#add_preference" do
+    it "adds a preference" do
+      visitor1 = Visitor.new('Bruce', 54, '$10')
+
+      visitor1.add_preference(:gentle)
+      visitor1.add_preference(:thrilling)
+
+      expect(visitor1.preferences).to eq([:gentle, :thrilling])
+    end
   end
 end
