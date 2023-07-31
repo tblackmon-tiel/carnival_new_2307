db46e5715a5703c069089f1c480ced855defd968
Tyler Blackmon <tyler.blackmon8@gmail.com>
2023-07-31 09:25:32 -0600

    feat: add details hash to ride

--- lib/ride.rb
+++ lib/ride.rb
@@ -1,5 +1,13 @@
 class Ride
+  attr_reader :name,
+              :min_height,
+              :admission_fee,
+              :excitement
+              
   def initialize(ride_details)
-    
+    @name = ride_details[:name]
+    @min_height = ride_details[:min_height]
+    @admission_fee = ride_details[:admission_fee]
+    @excitement = ride_details[:excitement]
   end
 end
--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -8,5 +8,14 @@ RSpec.describe Ride do
 
       expect(ride1).to be_a Ride
     end
+
+    it "has a hash of details" do
+      ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+      
+      expect(ride1.name).to eq("Carousel")
+      expect(ride1.min_height).to eq(24)
+      expect(ride1.admission_fee).to eq(1)
+      expect(ride1.excitement).to eq(:gentle)
+    end
   end
 end
