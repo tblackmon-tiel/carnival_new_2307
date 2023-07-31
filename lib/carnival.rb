a80b2326ed67fed4978a99450935e96bed861d84
Tyler Blackmon <tyler.blackmon8@gmail.com>
2023-07-31 11:02:31 -0600

    feat: add most_popular_ride to carnival

--- lib/carnival.rb
+++ lib/carnival.rb
@@ -9,4 +9,10 @@ class Carnival
   def add_ride(ride)
     @rides << ride
   end
+
+  def most_popular_ride
+    @rides.max do |a, b|
+      a.times_ridden? <=> b.times_ridden?
+    end
+  end
 end
--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -8,6 +8,13 @@ RSpec.describe Carnival do
     @ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
     @ride2 = Ride.new({ name: 'Ferris Wheel', min_height: 36, admission_fee: 5, excitement: :gentle })
     @ride3 = Ride.new({ name: 'Roller Coaster', min_height: 54, admission_fee: 2, excitement: :thrilling })
+    @visitor1 = Visitor.new('Bruce', 54, '$10')
+    @visitor2 = Visitor.new('Tucker', 36, '$5')
+    @visitor3 = Visitor.new('Penny', 64, '$15')
+    @visitor1.add_preference(:gentle)
+    @visitor2.add_preference(:gentle)
+    @visitor2.add_preference(:thrilling)
+    @visitor3.add_preference(:thrilling)
   end
 
   describe "#initialize" do
@@ -29,6 +36,19 @@ RSpec.describe Carnival do
       @carnival1.add_ride(@ride3)
 
       expect(@carnival1.rides).to eq([@ride1, @ride2, @ride3])
+    end
+  end
+
+  describe "#most_popular_ride" do
+    it "returns the most ridden ride" do
+      2.times { @ride1.board_rider(@visitor1) }
+      3.times { @ride3.board_rider(@visitor3) }
+      @carnival1.add_ride(@ride1)
+      @carnival1.add_ride(@ride2)
+      @carnival1.add_ride(@ride3)
+
+
+      expect(@carnival1.most_popular_ride).to eq(@ride3)
     end
   end
 end
