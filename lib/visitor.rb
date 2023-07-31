3a2464e837f9dcdd65f4e9f2e5940d017d06e0a8
Tyler Blackmon <tyler.blackmon8@gmail.com>
2023-07-31 09:12:52 -0600

    feat: add name, height, spending_money to visitor

--- lib/visitor.rb
+++ lib/visitor.rb
@@ -1,5 +1,11 @@
 class Visitor
+  attr_reader :name,
+              :height,
+              :spending_money
+              
   def initialize(name, height, spending_money)
-
+    @name = name
+    @height = height
+    @spending_money = spending_money
   end
 end
--- spec/visitor_spec.rb
+++ spec/visitor_spec.rb
@@ -7,5 +7,13 @@ RSpec.describe Visitor do
 
       expect(visitor1).to be_a Visitor
     end
+
+    it "has a name, height and spending money" do
+      visitor1 = Visitor.new('Bruce', 54, '$10')
+
+      expect(visitor1.name).to eq("Bruce")
+      expect(visitor1.height).to eq(54)
+      expect(visitor1.spending_money).to eq("$10")
+    end
   end
 end
